9919f9e059586ca74a6d8ede4dc6c61d722ee60d
zhong <zhonglikui@prprlive.com>
2017-08-02 19:47:05 +0800

    添加Retrofit调用的示例代码

---
+++ app/src/main/java/com/zhong/cardinals/sample/BaseApplication.java
@@ -0,0 +1,21 @@
+package com.zhong.cardinals.sample;
+
+import android.app.Application;
+
+import com.zhong.cardinals.App;
+import com.zhong.cardinals.net.NetWorkClient;
+
+/**
+ * Created by zhong on 2017/8/2.
+ */
+
+public class BaseApplication extends Application {
+    private static final String HOST = "http://";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        App.getInstance().init(this, true);
+        NetWorkClient.init(HOST);
+    }
+}
--- cardinals/src/main/java/com/zhong/cardinals/base/BaseCallback.java
+++ cardinals/src/main/java/com/zhong/cardinals/base/BaseCallback.java
@@ -1,37 +1,43 @@
 package com.zhong.cardinals.base;
 
+import java.io.IOException;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 /**
- * Created by zhong on 2017/3/28.
- * Retrofit的回调的封装形式
+ * Created by zhong on 2017/3/20.
  */
 
-public abstract class BaseCallback<M> implements Callback<M> {
-    public abstract void onSuccess(M model);
+public abstract class BaseCallback<T> implements Callback<T> {
+    public abstract void onSuccess(T model);
 
     public abstract void onFailure(int code, String msg);
-
-    public abstract void onThrowable(Throwable t);
 
     public abstract void onFinish();
 
     @Override
-    public void onResponse(Call<M> call, Response<M> response) {
-        if (response.isSuccessful()) {
+    public void onResponse(Call<T> call, Response<T> response) {
+        if (response.isSuccessful()) {//请求成功
             onSuccess(response.body());
-        } else {
-            onFailure(response.code(), response.errorBody().toString());
+        } else {//请求失败比如 404 500 之类的
+            try {
+                int code = response.code();
+                String error = response.errorBody().string();
+                onFailure(code, error);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
         onFinish();
 
     }
 
     @Override
-    public void onFailure(Call<M> call, Throwable t) {
-        onThrowable(t);
+    public void onFailure(Call<T> call, Throwable t) {//没有网络或者数据模型出错之类的
+        onFailure(-1, t.getMessage());
         onFinish();
 
     }
